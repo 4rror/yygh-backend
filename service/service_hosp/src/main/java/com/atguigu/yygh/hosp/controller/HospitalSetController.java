78f2186dc10e5fae0dcd864a9eb543845081e82b
haisky <levlaner@foxmail.com>
2023-06-25 18:40:07 +0800

    集成swagger2

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -2,6 +2,8 @@ package com.atguigu.yygh.hosp.controller;
 
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +11,7 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
+@Api(description = "医院设置接口")
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
 
@@ -16,12 +19,14 @@ public class HospitalSetController {
     private HospitalSetService hospitalSetService;
 
     // 查询所有医院设置
+    @ApiOperation(value = "医院设置列表")
     @GetMapping("findAll")
     public List<HospitalSet> findAll() {
         List<HospitalSet> list = hospitalSetService.list();
         return list;
     }
 
+    @ApiOperation(value = "医院设置删除")
     @DeleteMapping("{id}")
     public boolean removeById(@PathVariable Long id) {
         return hospitalSetService.removeById(id);
