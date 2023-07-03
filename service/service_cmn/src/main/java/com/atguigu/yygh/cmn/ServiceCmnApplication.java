e43e43ef837a5d9d0056875363800032ccea0d5b
haisky <levlaner@foxmail.com>
2023-07-03 11:36:31 +0800

    整合springboot alibaba

--- service/service_cmn/src/main/java/com/atguigu/yygh/cmn/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/ServiceCmnApplication.java
@@ -2,12 +2,16 @@ package com.atguigu.yygh.cmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author haisky
  */
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableTransactionManagement
 @ComponentScan(basePackages = {"com.atguigu"})
--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -2,8 +2,12 @@ package com.atguigu.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackages = "com.atguigu")
 public class ServiceHospApplication {
